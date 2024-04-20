75eda7344e45867abc82cee78d521f259c1537df
Guy-leroc Ossebi <thonerguyleroc1@gmail.com>
2024-04-20 15:16:27 -0400

    itinial commit

---
+++ secureapp/src/test/java/com/guyleroc/secureapp/SecureappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyleroc.secureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
